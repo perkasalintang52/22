87d22a1bfd1858ce725859b22979990b26fdb3de
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-02-28 14:18:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281368971807'] // Put your number here
+global.owner = ['6281322577123'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -29,8 +29,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'By'
-global.author = ' Vlynn'
-global.email = 'venzvl66@gmail.com'
+global.author = ' Mr. Bot Lintang'
+global.email = 'joceylintang12345@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wm = '© Vlynn-Md'
 global.media = 'https://telegra.ph/file/fb94b67e8562c0be33235.jpg'
